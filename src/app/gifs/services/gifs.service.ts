3dd0d49cd26f3eb5823a72eb1dc067597cb96d97
Luis Carlos Rodriguez Diaz <luiscrd9811@gmail.com>
2022-02-05 09:33:12 -0500

    gifApp

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,48 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Gif, SearchGifsResponse } from '../interface/gifs.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GifsService {
+
+  private servicioUrl:string = 'https://api.giphy.com/v1/gifs';
+  private apiKey:string = 'VfMVxJK41eh9eQgVkOQ8D6HZt1QuXeSo'
+  private _historial:string[]=[];
+  public resultados: Gif[] = [];
+
+  get historial(){
+    return[...this._historial];
+  }
+
+  constructor(private http:HttpClient){
+    this._historial=JSON.parse(localStorage.getItem('historial')!) || [];
+    this.resultados=JSON.parse(localStorage.getItem('respuesta')!) || [];
+
+  }
+
+  buscarGifs(query:string){
+
+    query=query.trim().toLocaleLowerCase();
+
+    if (!this._historial.includes(query)){
+      this._historial.unshift(query);
+      this._historial=this._historial.splice(0,10);
+
+      localStorage.setItem('historial', JSON.stringify(this._historial));
+    }
+
+    const params = new HttpParams()
+                                  .set('api_key',this.apiKey)
+                                  .set('limit','10')
+                                  .set('q',query);
+
+    this.http.get<SearchGifsResponse>(`${this.servicioUrl}/search`,{params})
+      .subscribe(( resp )=>{
+        this.resultados=resp.data;
+        localStorage.setItem('resultados', JSON.stringify(this.resultados));
+      })
+  }
+
+}
